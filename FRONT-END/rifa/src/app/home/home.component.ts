60df48d5ba7f33a1e5f003e9aab5d7024d8233e3
Reinaldo <00001110305230sp@al.educacao.sp.gov.br>
2025-08-27 19:02:24 -0300

    paginação

--- FRONT-END/rifa/src/app/home/home.component.ts
+++ FRONT-END/rifa/src/app/home/home.component.ts
@@ -3,32 +3,81 @@ import { ApiService } from '../api.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-    standalone: true,
-    imports:[FormsModule, CommonModule]
+  standalone: true,
+  imports: [FormsModule, CommonModule]
 })
 export class HomeComponent implements OnInit {
   numbers = Array.from({ length: 400 }, (_, i) => i + 1);
   marcados = new Set<number>();
-  perRow = 25;
+  perRow = 15;
+  currentPage = 0;
+  pageSize = 50; // só para celulares/small
+  paginatedNumbers: number[] = [];
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
     this.calcCols();
-    window.addEventListener('resize', () => this.calcCols());
+    this.calcPagination();
+    window.addEventListener('resize', () => {
+      this.calcCols();
+      this.calcPagination();
+    });
     this.loadMarcados();
   }
-
+  getTotalPages(): number {
+    return Math.ceil(this.numbers.length / this.pageSize);
+  }
+  
   calcCols() {
     const w = window.innerWidth;
     if (w <= 480) this.perRow = 5;
     else if (w < 768) this.perRow = 10;
-    else this.perRow = 25;
+    else this.perRow = 15;
+  }
+
+  calcPagination() {
+    const w = window.innerWidth;
+  
+    if (w <= 480) {
+      this.perRow = 5;
+    } else if (w < 768) {
+      this.perRow = 10;
+    } else {
+      this.perRow = 15;
+    }
+  
+    if (w <= 768) {
+      // Celulares/small: 10 linhas por página
+      const linhasVisiveis = 10;
+      this.pageSize = this.perRow * linhasVisiveis;
+    } else {
+      // Telas médias/grandes: 150 números por página
+      this.pageSize = 150;
+    }
+  
+    const start = this.currentPage * this.pageSize;
+    const end = start + this.pageSize;
+    this.paginatedNumbers = this.numbers.slice(start, end);
+  }
+  
+
+  nextPage() {
+    if ((this.currentPage + 1) * this.pageSize < this.numbers.length) {
+      this.currentPage++;
+      this.calcPagination();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.calcPagination();
+    }
   }
 
   loadMarcados() {
@@ -37,5 +86,7 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  isSelected(n: number) { return this.marcados.has(n); }
+  isSelected(n: number) {
+    return this.marcados.has(n);
+  }
 }
